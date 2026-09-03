2490153a42dfa5238702d5e96a8c29dd8350cf6c
NicoRepa <nicolasreparaz@gmail.com>
2026-09-03 15:21:39 -0300

    main.py creado con funciones aun sin desarrollar

---
+++ main.py
@@ -0,0 +1,55 @@
+#carga e inicializacion de datos en memoria
+def inicializar_clubes():
+    pass
+def inicializar_jugadores():
+    pass
+
+#crud jugadores
+def crear_jugador():
+    pass
+def editar_jugador():
+    pass
+def eliminar_jugador():
+    pass
+def buscar_jugador():
+    pass
+def listar_jugadores():
+    pass
+
+#crud clubes
+def crear_club():
+    pass
+def editar_club():
+    pass
+def eliminar_club():
+    pass
+def buscar_club():
+    pass
+def listar_clubes():
+    pass
+
+#filtros
+def filtrar_jugadores_por_edad():
+    pass
+def filtrar_jugadores_por_club():
+    pass
+def filtrar_jugadores_por_valor():
+    pass
+
+#estadisticas
+def calcular_promedio_edad_por_liga():
+    pass
+def calcular_promedio_edad_por_club():
+    pass
+def calcular_valor_por_club():
+    pass
+def calcular_goleador():
+    pass
+
+#matrices
+def crear_matriz_planteles():
+    pass
+def crear_matriz_jugadores():
+    pass
+def mostrar_matrices():
+    pass 
